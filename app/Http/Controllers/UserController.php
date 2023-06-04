20940152831725720d61f5832e73a6466fc8aa46
Aristiklever R. Sousa <aristirodsousa@gmail.com>
2023-06-04 17:20:51 -0300

    Criando a rota e o método de criação de um usuário.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        return response(
+            User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => $request->inout('password'),
+            ])
+        );
+    }
 }
